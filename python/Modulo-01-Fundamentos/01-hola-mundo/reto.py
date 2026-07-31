bb02c196d92f4a00de4bb3ac4d4a8596e2919c41
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2026-07-31 14:11:07 -0600

    fi:

--- python/Modulo-01-Fundamentos/01-hola-mundo/reto.py
+++ python/Modulo-01-Fundamentos/01-hola-mundo/reto.py
@@ -1,5 +1,5 @@
-print("Hola mundo")
-print("Hoy escribo mi primera línea.")
-print("Mañana construiré sistemas.")
-print("Fin de la transmisión.")
+print("---Hola mundo---")
+print("---Hoy escribo mi primera línea.---")
+print("---Mañana construiré sistemas.---")
+print("---Fin de la transmisión.---")
 
