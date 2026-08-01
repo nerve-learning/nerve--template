c770c5e04a5f8e16d31db91f4ed3ec1da467f26a
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2026-07-31 19:38:53 -0600

    Resolución de Módulo 05: Completar retos 41, 42, 43 y arreglar 50 para CI

---
+++ python/Modulo-05-Funciones/43-args-defecto/reto.py
@@ -0,0 +1,6 @@
+def preparar_cafe(tipo, azucar=2, tamano="Mediano"):
+    print(f"Preparando un {tipo} de tamaño {tamano} con {azucar} cucharadas de azúcar.")
+
+preparar_cafe("Latte")
+preparar_cafe("Expreso", 0)
+preparar_cafe("Moca", 5, "Grande")
