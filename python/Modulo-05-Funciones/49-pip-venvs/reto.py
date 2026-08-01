a6719b36f6b8ed4bf873b73dbe1670d83a63707c
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2026-07-31 19:32:38 -0600

    Resolución de Módulo 05: 49 pip venvs

---
+++ python/Modulo-05-Funciones/49-pip-venvs/reto.py
@@ -0,0 +1,7 @@
+import colorama
+
+colorama.init()
+
+print(colorama.Fore.YELLOW + "¡Atención! Este es un mensaje importante en amarillo.")
+print(colorama.Fore.CYAN + "El rey ha decretado un día festivo.")
+print(colorama.Fore.MAGENTA + "¡Larga vida al rey!")
